77660937845182644d2a37c70841f4c02a67feeb
Antal <Antal@DESKTOP-1BMPHRV.local>
2020-03-09 12:47:26 +0100

    .

---
+++ assignment/src/main/java/assignment/app/App.java
@@ -0,0 +1,10 @@
+package assignment.app;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ assignment/src/main/java/assignment/controller/UserController.java
@@ -0,0 +1,26 @@
+package assignment.controller;
+
+
+
+@Controller
+public class UserController {
+	
+	/*
+	@RequestMapping("/")
+	public String showIndexPage() {
+		return "index.html";
+	}
+	
+	
+
+	@RequestMapping("/selectLanguage")
+	public String languageSelection(@RequestParam String language) {
+		if (language.equals("java")) {
+			return "redirect:/404.html";
+		} else {
+
+			return "redirect:/c.html";
+		}
+	}
+*/
+}
