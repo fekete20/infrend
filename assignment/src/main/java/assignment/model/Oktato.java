f06d3e6a440bc950bd2bb65063a2eef2f1183512
Antal <Antal@DESKTOP-1BMPHRV.local>
2020-03-09 12:32:00 +0100

    Informatikai rendszerek építése beadandó (nem végleges).

--- assignment/src/main/java/assignment/model/Oktato.java
+++ assignment/src/main/java/assignment/model/Oktato.java
@@ -7,56 +7,41 @@ public class Oktato {
 	private String name;
 	private String tanszek;
 	private ArrayList<String> oktatott_targyak = new ArrayList<String>();
-	
-	
+
 	public Oktato(String name, String tanszek, ArrayList<String> oktatott_targyak) {
-		//super();
+		// super();
 		this.name = name;
 		this.tanszek = tanszek;
 		this.oktatott_targyak = oktatott_targyak;
 	}
-
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 
 	public String getTanszek() {
 		return tanszek;
 	}
 
-
 	public void setTanszek(String tanszek) {
 		this.tanszek = tanszek;
 	}
-
 
 	public ArrayList<String> getOktatott_targyak() {
 		return oktatott_targyak;
 	}
 
-
 	public void setOktatott_targyak(ArrayList<String> oktatott_targyak) {
 		this.oktatott_targyak = oktatott_targyak;
 	}
 
-
 	@Override
 	public String toString() {
-		return "Oktato [name=" + name + ", tanszek=" + tanszek + ", oktatott_targyak=" + oktatott_targyak + "]";
+		return "Oktató neve: " + name + ", oktatott tárgyai:" + oktatott_targyak;
 	}
-	
-	
-	
-	
-	
-	
-	
 
 }
