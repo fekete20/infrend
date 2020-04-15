fd2afa3ae66d0a05be3359f16328605eb0c31de5
fekete20 <Antal@DESKTOP>
2020-04-15 19:06:39 +0200

    .

--- infrend_assignment/src/infrend_assignment/Admin.java
+++ infrend_assignment/src/infrend_assignment/Admin.java
@@ -1,12 +1,36 @@
 package infrend_assignment;
 
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 public class Admin {
+	
+	@Id
+	private int id;
+	
+	@NotNull(message="A név nem lehet üres.")
 	private String name;
 
-	public Admin(String name) {
+	public Admin(String name, int id) {
 		// super();
 		this.name = name;
+		this.id= id;
+		
 	}
+
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
 
 	public String getName() {
 		return name;
--- infrend_assignment/src/infrend_assignment/Hallgato.java
+++ infrend_assignment/src/infrend_assignment/Hallgato.java
@@ -2,19 +2,45 @@ package infrend_assignment;
 
 import java.util.HashMap;
 
-public class Hallgato {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
+public class Hallgato {
+		
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		@Id
+		private int id;
+		
+		@NotNull(message = "A név nem lehet üres!")
 		private String name;
+		@NotNull(message = "A tankör nem lehet üres!")
 		private String tankor;
 		private HashMap<String, Integer> hallgatott_targyak = new HashMap<String, Integer>();
 
 //		private ArrayList<String> hallgatott_targyak = new ArrayList<String>();
-		public Hallgato(String name, String tankor, HashMap<String, Integer> hallgatott_targyak) {
+		public Hallgato(int id, String name, String tankor, HashMap<String, Integer> hallgatott_targyak) {
 			// super();
+			this.id = id;
 			this.name = name;
 			this.tankor = tankor;
 			this.hallgatott_targyak = hallgatott_targyak;
 		}
+
+		
+		
+		public int getId() {
+			return id;
+		}
+
+
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+
 
 		public String getName() {
 			return name;
