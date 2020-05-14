3199eafabea896c8545b308c9d1f1f0a8b57a4a9
fekete20 <Antal@DESKTOP>
2020-05-14 15:58:28 +0200

    Removed surplus files, little modifications again.

---
+++ infrend_assignment/sql_exported.sql
@@ -0,0 +1,47 @@
+create database neptun;
+use neptun;
+
+create table oktato (id int(9) auto_increment primary key, name varchar(255) not null, tanszek varchar(255) not null, oktatottTargyak varchar(255));
+
+-- néhány adat statikus felvitele
+insert into oktato values(1, 'oktato1', 'tanszek1', 'targy1');
+insert into oktato values(2, 'oktato2', 'tanszek2', 'targy2');
+insert into oktato values(3, 'oktato3', 'tanszek3', 'targy3');
+insert into oktato values(4, 'oktato4', 'tanszek4', 'targy2');
+insert into oktato values(5, 'oktato5', 'tanszek5', 'targy1');
+
+create table hallgato (id int(9) auto_increment primary key, name varchar(255) not null, tankor varchar(9) not null, hallgatottTargyak varchar(255), jegy int(1));
+
+-- néhány adat statikus felvitele
+insert into hallgato values(1, 'hallgato1', 'tankor1', 'targy1', 5);
+insert into hallgato values(2, 'hallgato2', 'tankor2', 'targy2', 4);
+insert into hallgato values(3, 'hallgato3', 'tankor2', 'targy3', 5);
+insert into hallgato values(4, 'hallgato4', 'tankor1', 'targy2', 2);
+insert into hallgato values(5, 'hallgato5', 'tankor1', 'targy1', 5);
+
+create table targy(tid int(9) auto_increment primary key, name varchar(255) not null);
+insert into targy values(1, 'targy1');
+insert into targy values(2, 'targy2');
+insert into targy values(3, 'targy3');
+insert into targy values(4, 'targy4');
+
+drop table oktato;
+drop table targy;
+
+create table oktato (oid int(9) auto_increment primary key, name varchar(255) not null, tanszek varchar(255) not null, tid int(9), foreign key(tid) references targy(tid));
+insert into oktato values(1, 'oktato1', 'tanszek1', 1);
+insert into oktato values(2, 'oktato2', 'tanszek2', 2);
+insert into oktato values(3, 'oktato3', 'tanszek3', 3);
+insert into oktato values(4, 'oktato4', 'tanszek4', 4);
+insert into oktato values(5, 'oktato5', 'tanszek5', 2);
+
+select tankor from hallgato group by tankor; -- tankorok lekerdezese
+select avg(jegy) from hallgato where tankor = 'tankor2';
+select tankor, avg(jegy) from hallgato group by tankor;
+
+select tankor, avg(jegy) from hallgato group by tankor
+
+select oid, oktato.name, tanszek, targy.name from oktato, targy;
+select * from oktato;
+select * from targy;
+insert into targy(tid, name) values (
---
+++ infrend_assignment/src/infrend_assignment/model/Atlag.java
@@ -0,0 +1,92 @@
+package infrend_assignment.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@SuppressWarnings("deprecation")
+@ManagedBean
+@RequestScoped
+public class Atlag {
+	private String tankor;
+	private Double avg;
+	
+	public Atlag(String tankor, Double avg) {
+		this.tankor = tankor;
+		this.avg = avg;
+	}
+	
+	public Atlag() {
+
+	}
+	
+	public String getTankor() {
+		return tankor;
+	}
+
+	public void setTankor(String tankor) {
+		this.tankor = tankor;
+	}
+
+	public Double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(Double atlag) {
+		this.avg = atlag;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Atlag [tankor=" + tankor + ", atlag=" + avg + ", connection=" + connection + "]";
+	}
+
+	private Connection connection;
+
+	public Connection getConnection() {
+		String username = "root";
+		String password = "root";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/neptun?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					username, password);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			return null;
+		}
+		return connection;
+	}
+	
+	ArrayList<Atlag> atlagok; 
+	public ArrayList<Atlag> atlagSzamitas() {
+		try {
+			connection = getConnection();
+			Statement stmt = getConnection().createStatement();
+			ResultSet rs = stmt.executeQuery("select tankor, avg(jegy) from hallgato group by tankor");
+			atlagok = new ArrayList<>();
+			while (rs.next()) {
+				Atlag atlag = new Atlag();
+				atlag.setTankor(rs.getString("tankor"));
+				atlag.setAvg(rs.getDouble("avg(jegy)"));
+				atlagok.add(atlag);				
+				
+				atlag.toString();
+			}	
+			connection.close();
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		return atlagok;
+	}
+	
+}
