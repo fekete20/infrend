f80739bf7a8c2b71e7958497dafab9e9faeb521c
fekete20 <Antal@DESKTOP>
2020-05-15 20:14:51 +0200

    Some bug fixed.

--- infrend_assignment/sql_exported.sql
+++ infrend_assignment/sql_exported.sql
@@ -3,19 +3,17 @@ use neptun;
 
 create table oktato (id int(9) auto_increment primary key, name varchar(255) not null, tanszek varchar(255) not null, oktatottTargyak varchar(255));
 
--- néhány adat statikus felvitele
-insert into oktato values(1, 'oktato1', 'tanszek1', 'targy1');
-insert into oktato values(2, 'oktato2', 'tanszek2', 'targy2');
-insert into oktato values(3, 'oktato3', 'tanszek3', 'targy3');
-insert into oktato values(4, 'oktato4', 'tanszek4', 'targy2');
-insert into oktato values(5, 'oktato5', 'tanszek5', 'targy1');
+-- nehany adat statikus felvitele
+insert into oktato values(1, 'Teszt Elek', 'Alkalmazot Informatikai Intézeti Tanszék', 'Infrend');
+insert into oktato values(2, 'Gipsz Jakab', 'Analízis Tanszék', 'Analízis I., Analízis II.');
+insert into oktato values(3, 'Barna Endre', 'Általános Informatikai Intézeti Tanszék', 'Számítógép hálózatok');
+insert into oktato values(4, 'Székely Balázs', 'Ábrázoló Geometria Tanszék', 'Ábrázoló Geometria, Bevezetés a CAD-be');
+
 
 create table hallgato (id int(9) auto_increment primary key, name varchar(255) not null, tankor varchar(9) not null, hallgatottTargyak varchar(255), jegy int(1));
 
--- néhány adat statikus felvitele
-insert into hallgato values(1, 'hallgato1', 'tankor1', 'targy1', 5);
-insert into hallgato values(2, 'hallgato2', 'tankor2', 'targy2', 4);
-insert into hallgato values(3, 'hallgato3', 'tankor2', 'targy3', 5);
-insert into hallgato values(4, 'hallgato4', 'tankor1', 'targy2', 1);
-insert into hallgato values(5, 'hallgato5', 'tankor1', 'targy1', 2);
-
+-- nehany adat statikus felvitele
+insert into hallgato values(1, 'Takács Attila', 'G3BIW', 'Infrend', 5);
+insert into hallgato values(2, 'Pásztor Péter', 'G3BIT', 'Analízis II.', 4);
+insert into hallgato values(3, 'Juhász Dénes', 'G3BIR', 'Bevezetés a CAD-be', 3);
+insert into hallgato values(4, 'Somogyi Attila', 'G3BIW', 'Számítógép hálózatok', 2);
